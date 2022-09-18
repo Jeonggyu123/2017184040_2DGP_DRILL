3d45020b38030d4a49419c61b8515bec6ea2770f
Jeonggyu123 <heroska1999@naver.com>
2022-09-18 17:43:41 +0900

    과제04

---
+++ Homework04/drill4.py.py
@@ -0,0 +1,57 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(500)
+turtle.forward
+<function forward at 0x0000018CDE3257E0>
+9fd
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    fd
+NameError: name 'fd' is not defined. Did you mean: 'id'?
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(500)
