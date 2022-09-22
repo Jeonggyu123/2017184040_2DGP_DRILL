4c9667763cfed8f313902def9fab7fe7f04e9e5a
Jeonggyu123 <heroska1999@naver.com>
2022-09-22 15:26:23 +0900

    과제06

---
+++ Homework06/drill6.py
@@ -0,0 +1,41 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\MyRepo\2017184040_2DGP_DRILL\Homework06\character_grass.py ====
+Pico2d is prepared.
+import os
+os.getcwd()
+'C:\\MyRepo\\2017184040_2DGP_DRILL\\Homework06'
+from pico2d import *
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+grass.draw_now(400, 30)
+character.draw_now(400, 90)
+import math
+
+x=0
+while (x < 800)
+SyntaxError: expected ':'
+while (x < 800):
+    x = x+2
+    delay(0.01)
+
+while (x < 800):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x, 90)
+    x = x + 2
+
+    
+while (x < 800):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x, 90)
+    x = x + 2
+    delay(0.01)
+
+math.sin(270/360*2*math.pi)
+-1.0
