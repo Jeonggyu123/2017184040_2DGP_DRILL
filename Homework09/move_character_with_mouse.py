e34397018f4f56bbfbf5c2f38bb674084174d779
Jeonggyu123 <heroska1999@naver.com>
2022-10-03 17:33:53 +0900

    과제09-1

--- Homework09/move_character_with_mouse.py
+++ Homework09/move_character_with_mouse.py
@@ -8,6 +8,7 @@ TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -56,6 +57,9 @@ def update_world():
         reset_world()
     pass
 
+
+framey=0
+
 reset_world()
 
 while running:
@@ -63,10 +67,14 @@ while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(ax, ay)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * framey, 100, 100, x, y)
     update_canvas()
-    frame = (frame + 1) % 8
 
+    frame = (frame + 1) % 8
+    if ax-x<0:          # frame (0,0), (0, 100), (0, 200), (0, 300)
+        framey=0
+    else :
+       framey=1
     handle_events()
 
 close_canvas()
